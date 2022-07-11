c9b467443048f5225407eab131ede4256bbea6ea
Vladyslav Panchenko <vladpanchenko280@ukr.net>
2022-07-11 12:48:42 +0300

    commit2

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './styles/index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/OnlineStore'>
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
